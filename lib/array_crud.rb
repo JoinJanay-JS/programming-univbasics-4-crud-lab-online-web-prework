a4818e621032a1a4ee470c3856732d6a5ee6b4b0
Janay Bailey <tjanay87@gmail.com>
2019-08-25 13:58:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   school = [ "study", "test", "practice", "experience"]
 end
 def add_element_to_end_of_array(array, element)
-  create_an_array << "set goals"
+  array << element
 end
 #
 # def add_element_to_start_of_array(array, element)
