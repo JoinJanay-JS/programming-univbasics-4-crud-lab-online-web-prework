fce0c79ee8bd873252ac2893723a04297792439b
Janay Bailey <tjanay87@gmail.com>
2019-08-25 13:56:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
- [] 
+ []
 end
 
 def create_an_array
@@ -9,32 +9,32 @@ end
 # def add_element_to_end_of_array(array, element)
 #   create_an_array << "set goals"
 # end
-# 
+#
 # def add_element_to_start_of_array(array, element)
 #   create_an_array.unshift("write it down")
 # end
-# 
+#
 # def remove_element_from_end_of_array(array)
 #   "test" = create_an_array.pop
 # end
-# 
+#
 # def remove_element_from_start_of_array(array)
 #   "study" = create_an_array.shift
 # end
-# 
+#
 # def retrieve_element_from_index(array, index_number)
 #   create_an_array= [2]
 # end
-# 
+#
 # def retrieve_first_element_from_array(array)
 #   create_an_array=[0]
 # end
-# 
+#
 # def retrieve_last_element_from_array(array)
 #   create_an_array=[-2]
 # end
-# 
+#
 # def update_element_from_index(array, index_number, element)
-# create_an_array=[2] 
+# create_an_array=[2]
 # => "don't give up"
 # end
