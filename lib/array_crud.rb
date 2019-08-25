d1c6e1c589c6b5d03f23e204266b67bdb2c0e7d1
Janay Bailey <tjanay87@gmail.com>
2019-08-25 13:56:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,37 +3,38 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  school = [ "study", "test", "practice", "experience"]
 end
-
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
-end
+# *
+# def add_element_to_end_of_array(array, element)
+#   create_an_array << "set goals"
+# end
+# 
+# def add_element_to_start_of_array(array, element)
+#   create_an_array.unshift("write it down")
+# end
+# 
+# def remove_element_from_end_of_array(array)
+#   "test" = create_an_array.pop
+# end
+# 
+# def remove_element_from_start_of_array(array)
+#   "study" = create_an_array.shift
+# end
+# 
+# def retrieve_element_from_index(array, index_number)
+#   create_an_array= [2]
+# end
+# 
+# def retrieve_first_element_from_array(array)
+#   create_an_array=[0]
+# end
+# 
+# def retrieve_last_element_from_array(array)
+#   create_an_array=[-2]
+# end
+# 
+# def update_element_from_index(array, index_number, element)
+# create_an_array=[2] 
+# => "don't give up"
+# end
